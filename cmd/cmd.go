11912f7ca51405fb8a64da5f896706e316b45682
Stephen Searles <stephen.searles@gmail.com>
2014-03-02 13:32:26 -0800

    Made the default command more useful for testing

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,20 +7,34 @@ import (
 	"os"
 
 	"stephensearles.com/php"
-	"stephensearles.com/php/passes/typechecking"
+	//"stephensearles.com/php/passes/typechecking"
+	"stephensearles.com/php/passes/printing"
 )
 
 func main() {
 	flag.Parse()
+	fmt.Println(flag.Arg(0))
 	fBytes, err := ioutil.ReadFile(flag.Arg(0))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	walker := printing.Walker{}
 	parser := php.NewParser(string(fBytes))
-	nodes := parser.Parse()
-	walker := typecheck.Walker{}
-	for _, node := range nodes {
-		walker.Walk(node)
+	nodes, errs := parser.Parse()
+	if len(errs) != 0 {
+		if len(nodes) != 0 && nodes[0] != nil {
+			walker.Walk(nodes[0])
+		}
+		for _, err := range errs {
+			fmt.Println(err)
+		}
 	}
+	/*
+		  nodes := parser.Parse()
+				walker := typecheck.Walker{}
+				for _, node := range nodes {
+					walker.Walk(node)
+				}
+	*/
 }
--- passes/printing/printing.go
+++ passes/printing/printing.go
@@ -13,13 +13,22 @@ type Walker struct {
 }
 
 func (w *Walker) Walk(node ast.Node) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			fmt.Printf("%T - %+v\n", node, node)
+			panic(r)
+		}
+	}()
 	fmt.Printf("%s(%T)%s\n", strings.Repeat("\t", w.tabLevel), node, node.String())
 	switch children := node.Children(); children {
 	case nil:
 	default:
 		w.tabLevel += 1
 		for _, child := range children {
-			w.Walk(child)
+			if child != nil {
+				w.Walk(child)
+			}
 		}
 		w.tabLevel -= 1
 	}
