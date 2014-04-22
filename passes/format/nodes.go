b1294729c142bba738816369baddf52525fe0fc2
Stephen Searles <stephen.searles@shipwire.com>
2014-04-22 10:00:41 -0700

    Improved core formatter code

--- passes/format/nodes.go
+++ passes/format/nodes.go
@@ -1,90 +1,28 @@
 package format
 
 import (
-	"io"
-	"strings"
-
 	"stephensearles.com/php/ast"
 	"stephensearles.com/php/token"
 )
 
-func (f *formatWalker) Walk(node ast.Node) error {
-	switch n := node.(type) {
-	case ast.ExpressionStmt:
-		f.Walk(n.Expression)
-		f.printToken(token.StatementEnd)
-		f.print("\n")
-	case *ast.Variable:
-		f.printToken(token.VariableOperator)
-		f.Walk(n.Name)
-	case ast.Variable:
-		f.printToken(token.VariableOperator)
-		f.Walk(n.Name)
-	case ast.Identifier:
-		f.print(n.Value)
-	case *ast.Literal:
-		f.print(n.Value)
-	case ast.Literal:
-		f.print(n.Value)
-	case ast.EchoStmt:
-		f.printEcho(&n)
-	case *ast.EchoStmt:
-		f.printEcho(n)
-	case ast.IfStmt:
-		f.printIf(&n)
-	case *ast.IfStmt:
-		f.printIf(n)
-	case *ast.Block:
-		for _, stmt := range n.Statements {
-			f.Walk(stmt)
-		}
-	}
-	return nil
-}
-
-func (f *formatWalker) print(s string) {
-	io.WriteString(f.w, s)
-}
-
-func (f *formatWalker) printToken(t token.Token) {
-	if s, ok := tokenMap[t]; ok {
-		io.WriteString(f.w, s)
-		return
-	}
-	io.WriteString(f.w, t.String())
-}
-
-func (f *formatWalker) printTab() {
-	io.WriteString(f.w, strings.Repeat(f.Indent, f.tabLevel))
-}
-
 func (f *formatWalker) printIf(node *ast.IfStmt) error {
 	f.printTab()
-	f.printToken(token.If)
-	f.print(" ")
-	f.printToken(token.OpenParen)
-	f.Walk(node.Condition)
-	f.printToken(token.CloseParen)
-	f.print(" ")
-	f.printToken(token.BlockBegin)
-	f.print("\n")
+	f.printf("if (%?) {", node.Condition)
+	f.println()
 	f.tabLevel += 1
 	f.Walk(node.TrueBranch)
-	f.print("\n")
 	f.tabLevel -= 1
+	f.printTab()
 	f.printToken(token.BlockEnd)
-	if node.FalseBranch != nil {
-		f.print(" ")
-		f.printToken(token.Else)
-		f.print(" ")
-		f.printToken(token.BlockBegin)
-		f.print("\n")
+	if node.FalseBranch != nil && len(node.FalseBranch.Children()) != 0 {
+		f.printf(" else {\n")
 		f.tabLevel += 1
 		f.Walk(node.FalseBranch)
 		f.tabLevel -= 1
-		f.print("\n")
-		f.printToken(token.BlockEnd)
-		f.print("\n")
+		f.printTab()
+		f.printf("}\n")
+	} else {
+		f.println()
 	}
 	return nil
 }
---
+++ passes/format/printer.go
@@ -0,0 +1,102 @@
+package format
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"stephensearles.com/php/ast"
+	"stephensearles.com/php/token"
+)
+
+func (f *formatWalker) Walk(node ast.Node) error {
+	f.printNode(node)
+	return nil
+}
+
+func (f *formatWalker) printNode(node ast.Node) {
+	switch n := node.(type) {
+	case ast.ExpressionStmt:
+		f.Walk(n.Expression)
+		f.printToken(token.StatementEnd)
+		f.print("\n")
+	case *ast.Variable:
+		f.printToken(token.VariableOperator)
+		f.Walk(n.Name)
+	case ast.Variable:
+		f.printToken(token.VariableOperator)
+		f.Walk(n.Name)
+	case ast.Identifier:
+		f.print(n.Value)
+	case *ast.Literal:
+		f.print(n.Value)
+	case ast.Literal:
+		f.print(n.Value)
+	case ast.EchoStmt:
+		f.printEcho(&n)
+	case *ast.EchoStmt:
+		f.printEcho(n)
+	case ast.IfStmt:
+		f.printIf(&n)
+	case *ast.IfStmt:
+		f.printIf(n)
+	case *ast.Block:
+		for _, stmt := range n.Statements {
+			f.Walk(stmt)
+		}
+	}
+}
+
+func (f *formatWalker) printf(fmtString string, i ...interface{}) {
+	num := 0
+	inPercent := false
+	for _, r := range fmtString {
+		if inPercent && r == '?' {
+			switch n := i[num].(type) {
+			case token.Token:
+				f.printToken(n)
+				num += 1
+			case ast.Node:
+				f.printNode(n)
+				num += 1
+			default:
+				fmt.Fprint(f.w, n)
+			}
+		} else if inPercent && r == '%' {
+			io.WriteString(f.w, "%")
+		} else if inPercent {
+			fmt.Fprintf(f.w, "%"+string(byte(r)), i[num])
+		} else if r == '%' {
+			inPercent = true
+			continue
+		} else {
+			fmt.Fprint(f.w, string(byte(r)))
+		}
+		inPercent = false
+	}
+}
+
+func (f *formatWalker) print(s string) {
+	io.WriteString(f.w, s)
+}
+
+func (f *formatWalker) printToken(t token.Token) {
+	if s, ok := tokenMap[t]; ok {
+		io.WriteString(f.w, s)
+		return
+	}
+	io.WriteString(f.w, t.String())
+}
+
+func (f *formatWalker) printTab() {
+	io.WriteString(f.w, strings.Repeat(f.Indent, f.tabLevel))
+}
+
+func (f *formatWalker) printTabbedLine() {
+	f.println()
+	f.printTab()
+}
+
+func (f *formatWalker) println() {
+	io.WriteString(f.w, "\n")
+}
