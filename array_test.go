3562c8f37126a43ebed58927688037a945bcf865
Stephen Searles <stephen.searles@gmail.com>
2014-07-27 10:24:49 -0700

    Migrated array bracket test to an actual unit test

---
+++ array_test.go
@@ -0,0 +1,55 @@
+package php
+
+import (
+	"testing"
+
+	"stephensearles.com/php/ast"
+)
+
+func TestArrayBracket(t *testing.T) {
+	testStr := `<?
+    $arr = ["one", "two"];
+    $arr2 = ["one" => 1, "two" => 2];`
+
+	p := NewParser(testStr)
+	a, errs := p.Parse()
+	if len(errs) != 0 {
+		t.Fatalf("Did not parse array bracket correctly: %s", errs)
+	}
+
+	tree := []ast.Statement{
+		ast.ExpressionStmt{ast.AssignmentExpression{
+			Operator: "=",
+			Assignee: ast.NewVariable("arr"),
+			Value: &ast.ArrayExpression{
+				Pairs: []ast.ArrayPair{
+					{Key: nil, Value: &ast.Literal{Value: `"one"`, Type: ast.String}},
+					{Key: nil, Value: &ast.Literal{Value: `"two"`, Type: ast.String}},
+				},
+			},
+		}},
+		ast.ExpressionStmt{ast.AssignmentExpression{
+			Operator: "=",
+			Assignee: ast.NewVariable("arr2"),
+			Value: &ast.ArrayExpression{
+				Pairs: []ast.ArrayPair{
+					{
+						Key:   &ast.Literal{Value: `"one"`, Type: ast.String},
+						Value: &ast.Literal{Value: "1", Type: ast.Float},
+					},
+					{
+						Key:   &ast.Literal{Value: `"two"`, Type: ast.String},
+						Value: &ast.Literal{Value: "2", Type: ast.Float},
+					},
+				},
+			},
+		}},
+	}
+
+	if !assertEquals(a[0], tree[0]) {
+		t.Fatalf("Array bracked did not parse correctly")
+	}
+	if !assertEquals(a[1], tree[1]) {
+		t.Fatalf("Array bracked did not parse correctly")
+	}
+}
--- testfiles/arrayBracket.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$arr = ["one", "two"];
-$arr2 = ["one" => 1, "two" => 2];
