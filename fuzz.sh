b6c8d683bbb1d64ae19cf55d3ee9be009361e234
Stephen Searles <stephen.searles@gmail.com>
2016-12-23 21:25:51 -0800

    fixing fuzz script

--- fuzz.sh
+++ fuzz.sh
@@ -5,5 +5,5 @@ set -e
 go get github.com/dvyukov/go-fuzz/go-fuzz
 go get github.com/dvyukov/go-fuzz/go-fuzz-build
 
-go-fuzz-build github.com/stephens2424/php
-go-fuzz -bin php-fuzz.zip -workdir testdata/fuzzdir -masterstats ":1234"
+go-fuzz-build github.com/stephens2424/php/parser
+go-fuzz -bin php-fuzz.zip -workdir testdata/fuzzdir -master ":1234"
